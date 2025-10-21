6668ecd3fd5eccb6e4e603f93f40205991f739af
koouvip <koouvip@126.com>
2025-10-21 10:36:09 +0800

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InventorSignatureAddIn")]
+[assembly: AssemblyDescription("Autodesk Inventor 2021 电子签名插件")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("您的公司")]
+[assembly: AssemblyProduct("InventorSignatureAddIn")]
+[assembly: AssemblyCopyright("版权所有 © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(true)]
+[assembly: Guid("12345678-1234-1234-1234-123456789ABC")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
